fdca1344940c4b51c8201ade0a2f074b140aa315
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-11-21 22:42:53 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -13,5 +13,11 @@ document.write(`
       <a href="/skydb/main.html" class="w3-bar-item w3-button">Main</a>
     </div>
   </div>
+  <div class="w3-dropdown-hover">
+    <button class="w3-button">Libraries</button>
+    <div class="w3-dropdown-content w3-bar-block w3-card-4">
+      <a href="/libs/sUtils/sUtils.html" class="w3-bar-item w3-button">sUtils</a>
+    </div>
+  </div>
 </div>
 `);
