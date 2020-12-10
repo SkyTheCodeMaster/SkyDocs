6c96a50a81bcf97ca325df93c51b9a98f5ccae58
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-12-10 09:38:43 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -4,6 +4,7 @@ document.write(`
   <div class="w3-dropdown-hover">
     <button class="w3-button">SkyOS</button>
     <div class="w3-dropdown-content w3-bar-block w3-card-4">
+      <a href="/skyos/main.html" class="w3-bar-tem w3-button">Main</a>
       <a href="/skyos/errorCodes.html" class="w3-bar-item w3-button">Error Codes</a>
     </div>
   </div>
