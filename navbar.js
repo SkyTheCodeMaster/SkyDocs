1f9547f0d285b1b93fcad79e5baeb93dc0417056
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-12-17 11:35:59 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,6 +1,7 @@
 document.write(`
 <div class="w3-bar w3-black">
   <a href="/index.html" class="w3-bar-item w3-button w3-mobile">Home</a>
+  <a href="https://github.com/SkyTheCodeMaster/SkyDocs" class="w3-bar-item w3-button w3-mobile">GH Repo</a>
   <div class="w3-dropdown-hover">
     <button class="w3-button">SkyOS</button>
     <div class="w3-dropdown-content w3-bar-block w3-card-4">
