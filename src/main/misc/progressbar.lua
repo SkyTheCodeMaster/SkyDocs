2306dd0a9be89bf26daab646dd0f864016da4427
= <owenpatrickdrysdale@gmail.com>
2021-06-04 15:29:24 +0000

    Fix math, allow calling create directly

--- src/main/misc/progressbar.lua
+++ src/main/misc/progressbar.lua
@@ -1,6 +1,8 @@
 --- progress bar is an api for drawing & updating progress bars.
 -- @module[kind=misc] progressbar
 
+local expect = require("cc.expect").expect
+
 local function save()
   local x,y = term.getCursorPos()
   local tbl = {
@@ -59,15 +61,17 @@ local function update(bar,percent)
   if percent > 100 then percent = 100 end
   local pixels = math.floor((percent / (100 / bar.w)) + 0.5) -- The math.floor + 0.5 acts as a rounding function.
   -- percent / (100 / barWidth) calculates how many pixels should be filled in the bar
-  dfb(bar.x,bar.y,(bar.x+bar.w),(bar.y+bar.h),bar.bg,bar.terminal)
+  dfb(bar.x,bar.y,(bar.x+bar.w-1),(bar.y+bar.h-1),bar.bg,bar.terminal)
   if pixels ~= 0 then
-    dfb(bar.x,bar.y,(bar.x+pixels),(bar.y+bar.h),bar.fg,bar.terminal)
+    dfb(bar.x,bar.y,(bar.x+pixels-1),(bar.y+bar.h-1),bar.fg,bar.terminal)
   end
   return percent
 end
 
 local bar = {} --- @type bar
-local mt = {["__index"] = bar}
+local mt = {
+  __index = bar
+}
 
 --- Update the bar to a percentage from 0 to 100.
 -- @tparam number percent Percentage of how full the bar is.
@@ -102,10 +106,12 @@ local function create(x,y,w,h,fg,bg,fill,terminal)
     fill = fill,
     terminal = terminal,
   }
-  update(bar,fill)
+  -- draw the background
+  dfb(x,y,x+w)
+  if fill ~= 0 then update(bar,fill) end
   return setmetatable(bar,mt)
 end
 
-return {
+return setmetatable({
   create = create,
-}
+}, {__call = function(_,...) return create(...) end})
