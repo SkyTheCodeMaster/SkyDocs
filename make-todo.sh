a166c8d09e33ebbefe3587373a1db62cbeae1cc4
= <owenpatrickdrysdale@gmail.com>
2021-07-07 03:47:00 +0000

    Fix actions

---
+++ make-todo.sh
@@ -0,0 +1,2 @@
+grep -Rni todo src/ build/docs/lua/scripts build/docs/lua/scriptdata > todo.txt
+# This does roughly the same thing as `todo.py`, but outputs it as a text document in `todo.txt`. Useful for chekcing the local todo instead of pushing :)
---
+++ src/main/skyos-events/pan.md
@@ -0,0 +1,7 @@
+---
+module: [kind=skyosevent] pan
+---
+This event is called when the user clicks and drags on the screen.
+## **Return Values**
+1. @{string}: The event name.
+2. @{string}: The direction being panned, `left`, `right`, `up`, `down`.
